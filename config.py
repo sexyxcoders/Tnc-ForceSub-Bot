490190871e97096e82acae83383a8593ca159cec
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-29 16:05:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from Tnc import *
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URL = os.getenv("MONGO_URL", "")
+API_ID = int(os.getenv("API_ID", "29308061"))
+API_HASH = os.getenv("API_HASH", "462de3dfc98fd938ef9c6ee31a72d099")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8471675718:AAFVo3-PLwUn4Ebhj1XS2McN8LbaUB1ZzpI")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://YoruMusic:Yoruu@yoru.ku15xlh.mongodb.net/?appName=Yoru")
 
 START_IMAGE = "assets/start.jpg"
 BOT_NAME = ""
