8e6fcae3f98df522b8ca07228734e4fe663276ca
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-29 15:49:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from Tnc import *
 import os
 
-API_ID = int(os.getenv("API_ID", "12345"))
-API_HASH = os.getenv("API_HASH", "your_api_hash_here")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
-MONGO_URL = os.getenv("MONGO_URL", "your_mongodb_connection_string_here")
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URL = os.getenv("MONGO_URL", "")
 
 START_IMAGE = "assets/start.jpg"
-BOT_NAME = "Tnc Must Join Bot"
-SUPPORT_CHAT = "https://t.me/TncSupport"
+BOT_NAME = ""
+SUPPORT_CHAT = "https://t.me/TNCmeetup"
 FORCE_CHANNEL = "@TechNodeCoders"
 
 OWNER_ID = 8487284125
