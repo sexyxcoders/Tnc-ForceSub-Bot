852c7e3edcc1b69c1f7f0787c69be400e2af6c9d
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-29 14:33:41 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,26 @@
+from Tnc import *
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import START_IMAGE, BOT_NAME, SUPPORT_CHAT, FORCE_CHANNEL, OWNER_ID
+from main import app
+
+@app.on_message(filters.command("start") & filters.private)
+async def start_cmd(_, m):
+    user = m.from_user
+    # check if user joined force channel
+    try:
+        member = await app.get_chat_member(FORCE_CHANNEL, user.id)
+        joined = member.status not in ["left", "kicked"]
+    except:
+        joined = False
+
+    if not joined and user.id != OWNER_ID:
+        btn = InlineKeyboardMarkup([[InlineKeyboardButton("📢 Join Channel", url="https://t.me/TechNodeCoders")]])
+        return await m.reply_text("❌ You must join {} first to use this bot.\n🔗 Join the channel and then press /start again.".format(FORCE_CHANNEL), reply_markup=btn)
+
+    caption = "💫 𝗧𝗡𝗖 𝗠𝗨𝗦𝗧 𝗝𝗢𝗜𝗡 𝗕𝗢𝗧\n👑 Made by ㅤ- 𝖳ɴᴄ //Ｉᴠ ⋏ ᥒ\n🔗 Must Join: {}".format(FORCE_CHANNEL)
+    # try to send as spoiler image (has_spoiler param if supported)
+    try:
+        await app.send_photo(m.chat.id, START_IMAGE, caption=caption, has_spoiler=True)
+    except TypeError:
+        await app.send_photo(m.chat.id, START_IMAGE, caption=caption)
