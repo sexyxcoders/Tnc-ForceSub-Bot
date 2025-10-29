3f6c89369efbf10bfbf0475f4612919ce93b900f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-29 14:34:36 +0530

    Add files via upload

---
+++ Tnc/constants.py
@@ -0,0 +1,11 @@
+BRAND_NAME = "Tnc"
+OWNER = "Tnc Developers"
+REPO = "https://github.com/Tnc-Official/ForceJoinBot"
+SUPPORT = "https://t.me/TncSupport"
+
+EMOJI_SUCCESS = "✅"
+EMOJI_FAIL = "❌"
+EMOJI_WARN = "⚠️"
+EMOJI_PING = "🏓"
+
+SEPARATOR = "━━━━━━━━━━━━━━━"
